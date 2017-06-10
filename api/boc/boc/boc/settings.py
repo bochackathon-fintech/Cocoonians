5e1dccbe25d74f6fd47242ee1443da1d44d3918b
Elena Georgiou Strouthos <elena@cocooncreations.net>
2017-06-10 11:54:54 +0300

    remove sqlite3

--- api/boc/boc/boc/settings.py
+++ api/boc/boc/boc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oojf*8#*!iqx&+@jm89zwu2e3-%*#0hx146*%^26xz2!bxi))q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.88.84"]
+ALLOWED_HOSTS = ["192.168.88.84", "127.0.0.1"]
 
 
 # Application definition
