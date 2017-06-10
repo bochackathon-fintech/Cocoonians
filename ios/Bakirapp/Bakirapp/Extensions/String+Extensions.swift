69d26b8d40dea7c300abd688d2c1961c680291c2
Elena Georgiou Strouthos <elena@cocooncreations.net>
2017-06-10 22:38:39 +0300

    accounts parsing

---
+++ ios/Bakirapp/Bakirapp/Extensions/String+Extensions.swift
@@ -0,0 +1,70 @@
+//
+//  String+Extensions.swift
+//  Bakirapp
+//
+//  Created by Elena Georgiou on 10/06/2017.
+//  Copyright © 2017 Cocoonians. All rights reserved.
+//
+
+import Foundation
+extension String {
+    var toDate:Date? {
+        //"31, Mar 2006";
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        
+        
+        dateFormatter.dateFormat = "dd, MMM yyyy - HH:mm:ssZ"
+        if let date =  dateFormatter.date(from: self) {
+            return date
+        }
+        
+        dateFormatter.dateFormat = "dd',' MMM yyyy - HH:mm'.'ZZZZ"
+        if let date = dateFormatter.date(from: self) {
+            return date
+        }
+        
+        dateFormatter.dateFormat = "dd',' MMM yyyy - HH:mm:ss"
+        if let date =  dateFormatter.date(from: self) {
+            
+            
+            return date
+        }
+        
+        //2017-06-10T09:41:27.200785Z
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        if let date =  dateFormatter.date(from: self) {
+            return date
+        }
+        
+        dateFormatter.dateFormat = "dd, MMM yyyy"
+        if let date =  dateFormatter.date(from: self) {
+            return date
+        }
+        
+        
+        dateFormatter.dateFormat = "HH:mm:ss"
+        if let date =  dateFormatter.date(from: self) {
+            return date
+        }
+        
+        
+        
+        //"27, Sep 2016 - 09:51.+0000"
+        
+        
+        
+        
+        
+        return nil
+    }
+    
+    
+    
+    
+    var nullify:String? {
+        return self.isEmpty ? nil : self
+    }
+    
+}
--- ios/Bakirapp/Bakirapp/Models/Account+Parse.swift
+++ ios/Bakirapp/Bakirapp/Models/Account+Parse.swift
@@ -20,26 +20,51 @@ extension Account {
             ]
             
             Alamofire.request(Feeds.accounts, method: .get, headers: headers).responseJSON { (response) in
-                
                 ContextManager.shared.privateContext.perform {
-                    if let JSON = response.result.value as? [String : Any]{
-                        guard let category_data = JSON["category"] as? [String:Any],
-                            let cat_code = category_data["code"] as? String,
-                            let cat_name = category_data["name"] as? String,
-                            let category = AccountCategory.create(context: ContextManager.shared.privateContext, code: cat_code, name: cat_name) else {
-                                completion(false, nil)
-                                return
+                    if let JSON = response.result.value as? [String : Any], let results = JSON["results"] as? [[String : Any]]{
+                        
+                        print(JSON)
+                        var accounts:[Account] = []
+                        for result in results{
+                            guard let category_data = result["category"] as? [String:Any],
+                                let cat_code = category_data["code"] as? String,
+                                let cat_name = category_data["name"] as? String,
+                                let category = AccountCategory.create(context: ContextManager.shared.privateContext, code: cat_code, name: cat_name) else {
+                                    completion(false, nil)
+                                    break
+                            }
+                            
+                            guard let currency_data = result["currency"] as? [String:Any],
+                                let cur_code = currency_data["code"] as? String,
+                                let cur_name = currency_data["name"] as? String,
+                                let currency = Currency.create(context: ContextManager.shared.privateContext, code: cur_code, name: cur_name) else {
+                                    completion(false, nil)
+                                    break
+                            }
+                            
+                            guard let owner_data = result["owner"] as? [String:Any],
+                                let customer_number = owner_data["customer_number"] as? String,
+                                let email = owner_data["email"] as? String,
+                                let first_name = owner_data["first_name"] as? String,
+                                let last_name = owner_data["last_name"] as? String,
+                                let owner = Customer.create(context: ContextManager.shared.privateContext, customer_number: customer_number, email: email, first_name: first_name, last_name: last_name) else {
+                                    completion(false, nil)
+                                    break
+                            }
+                            
+                            if let acount_number = result["acount_number"] as? String,
+                                let balance_str = result["balance"] as? String,
+                                let balance = Double(balance_str),
+                                let name = result["name"] as? String,
+                                let overdraft_limit_str = result["ovedraft_limit"] as? String,
+                                let overdraft_limit = Double(overdraft_limit_str),
+                                let opening_date = (result["opening_date"] as? String)?.toDate,
+                                let account = Account.create(context: ContextManager.shared.privateContext, acount_number: acount_number, balance: balance, category: category, currency: currency, name: name, opening_date: opening_date, overdraft_limit: overdraft_limit, owner: owner){
+                                
+                                accounts.append(account)
+                            }
                         }
-                        
-                        guard let currency_data = JSON["currency"] as? [String:Any],
-                            let cur_code = category_data["code"] as? String,
-                            let cur_name = category_data["name"] as? String,
-                            let currency = Currency.create(context: ContextManager.shared.privateContext, code: cur_code, name: cur_name) else {
-                                completion(false, nil)
-                                return
-                        }
-                        
-                        
+                        completion(true, accounts)
                     }
                 }
             }
